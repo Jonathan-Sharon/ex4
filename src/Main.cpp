db3a156ce012b800dfe49b675cd7a5e3829b8b8e
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-20 02:15:05 +0300

    fixed bug in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdexcept>
 
 #include "Communication/TCPCommunication.h"
@@ -6,11 +7,11 @@ using namespace TCPCommunication;
 
 int main(int argc, char *argv[])
 {
-    if (argc == 3)
-    {
-        connect(argc, argv);
-        return;
-    }
+  if (argc == 3 || argc == 2)
+  {
+    connect(argc, argv);
+    return 0;
+  }
 
-    throw std::runtime_error("invalid input!");
+  throw std::runtime_error("invalid input!");
 }
