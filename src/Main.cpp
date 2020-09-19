0c12dab927f2ab440251b7cec996aaa0bf90ec6e
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-19 20:11:08 +0300

    update the main to check if the input is valid

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,15 @@
+#include <stdexcept>
 #include "Communication/TCPCommunication.h"
 
 using namespace TCPCommunication;
 
-int main(int argc, char* argv[]) {
-    connect(argc, argv);
+int main(int argc, char *argv[])
+{
+    if (argc == 3)
+    {
+        connect(argc, argv);
+        return;
+    }
+
+    throw std::runtime_error("invalid input!");
 }
