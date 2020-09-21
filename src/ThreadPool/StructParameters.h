cc2b51267349b47a9f88258ce02bfee30ccb897b
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-22 02:57:39 +0300

    created a header which defines the parameters of Read/Write/Operation

---
+++ src/ThreadPool/StructParameters.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ctime>
+#include <memory>
+
+namespace ThreadPool
+{
+    struct readParameters
+    {
+        time_t lastReadTime;
+        int sockfd;
+    };
+
+    struct operateParameters
+    {
+        int sockfd;
+    };
+
+    struct writeParameters
+    {
+        std::unique_ptr<std::string> result;
+        double version;
+        int sockfd;
+        uint errorCode;
+    };
+} // namespace ThreadPool
