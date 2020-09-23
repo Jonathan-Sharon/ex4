8378936065cdff6bcff19ac700d8a58538e655a7
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-23 16:52:30 +0300

    Bug fixed in the Read/Write/Operate Creators classes

--- src/Factory/OperateCreator.cpp
+++ src/Factory/OperateCreator.cpp
@@ -7,26 +7,26 @@ void ThreadPool::DFSCreator::addToQueue(ThreadPool::Queue &queue,
                                         const ThreadPool::operateParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_operateQueueMutex);
-    queue.m_operateQueue.push({std::make_unique<Operate::DFS>(), info});
+    queue.m_operateQueue.push({std::make_shared<Operate::DFS>(), info});
 }
 
 void ThreadPool::BFSCreator::addToQueue(ThreadPool::Queue &queue,
                                         const ThreadPool::operateParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_operateQueueMutex);
-    queue.m_operateQueue.push({std::make_unique<Operate::BFS>(), info});
+    queue.m_operateQueue.push({std::make_shared<Operate::BFS>(), info});
 }
 
 void ThreadPool::BestFSCreator::addToQueue(ThreadPool::Queue &queue,
                                            const ThreadPool::operateParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_operateQueueMutex);
-    queue.m_operateQueue.push({std::make_unique<Operate::BestFS>(), info});
+    queue.m_operateQueue.push({std::make_shared<Operate::BestFS>(), info});
 }
 
 void ThreadPool::AStarCreator::addToQueue(ThreadPool::Queue &queue,
                                           const ThreadPool::operateParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_operateQueueMutex);
-    queue.m_operateQueue.push({std::make_unique<Operate::AStar>(), info});
+    queue.m_operateQueue.push({std::make_shared<Operate::AStar>(), info});
 }
--- src/Factory/ReadCreator.cpp
+++ src/Factory/ReadCreator.cpp
@@ -7,12 +7,12 @@ void ThreadPool::FirstReadCreator::addToQueue(ThreadPool::Queue &queue,
                                               const ThreadPool::readParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_readQueueMutex);
-    queue.m_readQueue.push({std::make_unique<Read::FirstRead>(), info});
+    queue.m_readQueue.push({std::make_shared<Read::FirstRead>(), info});
 }
 
 void ThreadPool::SecondReadCreator::addToQueue(ThreadPool::Queue &queue,
                                                const ThreadPool::readParameters info) const
 {
     std::unique_lock<std::mutex> lock(queue.m_readQueueMutex);
-    queue.m_readQueue.push({std::make_unique<Read::SecondRead>(), info});
+    queue.m_readQueue.push({std::make_shared<Read::SecondRead>(), info});
 }
