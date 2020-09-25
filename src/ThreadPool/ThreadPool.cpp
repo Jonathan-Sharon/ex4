9c31b8ee9bdb6826d3b8abaeb9b566ef1fad2b8b
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-26 02:41:56 +0300

    added close server function

--- src/ThreadPool/ThreadPool.cpp
+++ src/ThreadPool/ThreadPool.cpp
@@ -4,249 +4,257 @@
 
 #include "ThreadPool.h"
 
-#include "../Factory/WaitForReadCreator.h"
 #include "../Factory/ReadCreator.h"
+#include "../Factory/WaitForReadCreator.h"
 
 #include <algorithm>
 #include <functional>
 
 ThreadPool::Queue::Queue(const int sockfd, const sockaddr_in connectAddress,
-                         const unsigned int numberOfThreads) : m_mapCreator{std::make_unique<MapCreator::MapCreator>()},
-                                                               m_sockfd{sockfd},
-                                                               m_connectAddress{connectAddress},
-                                                               m_numberOfThreads{numberOfThreads},
-                                                               m_isSerial{numberOfThreads == 1},
-                                                               m_isThereSomethingToRead{false},
-                                                               m_numberOfAvailableThreads{numberOfThreads - 1}
-{
-}
+                         const unsigned int numberOfThreads)
+    : m_mapCreator{std::make_unique<MapCreator::MapCreator>()},
+      m_sockfd{sockfd}, m_connectAddress{connectAddress},
+      m_numberOfThreads{numberOfThreads}, m_isSerial{numberOfThreads == 1},
+      m_isThereSomethingToRead{false}, m_numberOfAvailableThreads{
+                                           numberOfThreads - 1} {}
 
-void ThreadPool::Queue::allocate()
-{
-    while (true)
-    {
+void ThreadPool::Queue::allocate() {
+  while (true) {
 
-        //Wait until there is operation to do with an available thread
-        wait();
+    // Wait until there is operation to do with an available thread
+    wait();
 
-        //If the write queue is not empty, there is a write operation to do.
-        if (!m_writeQueue.empty())
-        {
-            //If the server is serial, then call for the write operation
-            //in the queue using the main thread.
-            if (m_isSerial == true)
-            {
-                m_writeQueue.front().writer.get()->writeMessage(*this, m_writeQueue.front().parameters);
-                m_writeQueue.pop();
-                continue;
-            }
+    // If the write queue is not empty, there is a write operation to do.
+    if (!m_writeQueue.empty()) {
+      // If the server is serial, then call for the write operation
+      // in the queue using the main thread.
+      if (m_isSerial == true) {
+        m_writeQueue.front().writer.get()->writeMessage(
+            *this, m_writeQueue.front().parameters);
+        m_writeQueue.pop();
+        continue;
+      }
 
-            //The server is parallel. Since it's the only class that can create
-            //thread, and according to the wait() function there is a thread available
-            //then create a thread to do the Write operation. Also, update the
-            //number of available threads.
-            std::unique_lock<std::mutex> lock(m_writeQueueMutex);
-            m_threadVector.push_back(std::thread(writeThread, std::ref(*this), m_writeQueue.front()));
-            --m_numberOfAvailableThreads;
-            m_writeQueue.pop();
-            continue;
-        }
-
-        //If the Operate queue is not empty, there is a OPerate operation to do.
-        if (!m_operateQueue.empty())
-        {
-            //If the server is serial, then call for the operate operation
-            //in the queue using the main thread.
-            if (m_isSerial == true)
-            {
-                m_operateQueue.front().operation.get()->operate(*this, m_operateQueue.front().parameters);
-                m_operateQueue.pop();
-                continue;
-            }
-
-            //The server is parallel. Since it's the only class that can create
-            //thread, and according to the wait() function there is a thread available
-            //then create a thread to do the Operate operation. Also, update the
-            //number of available threads.
-            std::unique_lock<std::mutex> lock(m_operateQueueMutex);
-            m_threadVector.push_back(std::thread(operateThread, std::ref(*this), m_operateQueue.front()));
-            --m_numberOfAvailableThreads;
-            m_operateQueue.pop();
-            continue;
-        }
-
-        //If the Read queue is not empty, there is a Read operation to do.
-        if (!m_readQueue.empty())
-        {
-            //If the server is serial, then call for the Read operation
-            //in the queue using the main thread.
-            if (m_isSerial == true)
-            {
-                m_readQueue.front().reader.get()->readMessage(*this, m_readQueue.front().parameters);
-                m_readQueue.pop();
-                continue;
-            }
-
-            //The server is parallel. Since it's the only class that can create
-            //thread, and according to the wait() function there is a thread available
-            //then create a thread to do the Read operation. Also, update the
-            //number of available threads.
-            std::unique_lock<std::mutex> lock(m_readQueueMutex);
-            m_threadVector.push_back(std::thread(readThread, std::ref(*this), m_readQueue.front()));
-            --m_numberOfAvailableThreads;
-            m_readQueue.pop();
-            continue;
-        }
-    }
-}
-
-void ThreadPool::Queue::wait()
-{
-    while (true)
-    {
-        //Check if there is an available thread
-        if (m_isSerial == false && m_numberOfAvailableThreads <= 0)
-        {
-            continue;
-        }
-
-        if (m_numberOfAvailableThreads >= m_numberOfThreads)
-        {
-            throw std::runtime_error("number of available operation is bigger then number of threads");
-        }
-
-        //there is a Read operation to do.
-        if (!m_readQueue.empty())
-        {
-            return;
-        }
-
-        //there is an Operate operation to do.
-        if (!m_operateQueue.empty())
-        {
-            return;
-        }
-
-        //there is a Write operation to do.
-        if (!m_writeQueue.empty())
-        {
-            return;
-        }
-
-        checkActiveFd();
-
-        if (m_isSerial == true)
-        {
-            joinThreads();
-        }
-    }
-}
-
-void ThreadPool::Queue::checkActiveFd()
-{
-    fd_set readfds;
-    //clear the socket set
-    FD_ZERO(&readfds);
-
-    //add master socket to set
-    FD_SET(m_sockfd, &readfds);
-    int maxSd{m_sockfd};
-
-    //get the current time
-    std::time_t currentTime = std::time(nullptr);
-
-    {
-        std::unique_lock<std::mutex> lock(m_waitForReadVectorMutex);
-        for (int i{static_cast<int>(m_waitForReadVector.size() - 1)}; i >= 0; --i)
-        {
-            //Check if 5 seconds have passed.
-            //If so, close the connection
-            if (m_waitForReadVector[i].lastReadTime + TIME_TO_CLOSE <= currentTime)
-            {
-                m_waitForReadVector.erase(m_waitForReadVector.begin() + i);
-                close(m_waitForReadVector[i].sockfd);
-                continue;
-            }
-
-            //add to read list
-            FD_SET(m_waitForReadVector[i].sockfd, &readfds);
-
-            //highest file descriptor number, need it for the select function
-            if (m_waitForReadVector[i].sockfd > maxSd)
-            {
-                maxSd = m_waitForReadVector[i].sockfd;
-            }
-        }
+      // The server is parallel. Since it's the only class that can create
+      // thread, and according to the wait() function there is a thread
+      // available then create a thread to do the Write operation. Also, update
+      // the number of available threads.
+      std::unique_lock<std::mutex> lock(m_writeQueueMutex);
+      m_threadVector.push_back(
+          std::thread(writeThread, std::ref(*this), m_writeQueue.front()));
+      --m_numberOfAvailableThreads;
+      m_writeQueue.pop();
+      continue;
     }
 
-    //wait for an activity on one of the sockets , timeout is 0 ,
-    //so select() returns immediately
-    timeval timeout = {0, 0};
-    int activity = select(maxSd + 1, &readfds, NULL, NULL, &timeout);
+    // If the Operate queue is not empty, there is a OPerate operation to do.
+    if (!m_operateQueue.empty()) {
+      // If the server is serial, then call for the operate operation
+      // in the queue using the main thread.
+      if (m_isSerial == true) {
+        m_operateQueue.front().operation.get()->operate(
+            *this, m_operateQueue.front().parameters);
+        m_operateQueue.pop();
+        continue;
+      }
 
-    if (activity < 0)
-    {
-        close(m_sockfd);
-        throw std::system_error{errno, std::system_category()};
+      // The server is parallel. Since it's the only class that can create
+      // thread, and according to the wait() function there is a thread
+      // available then create a thread to do the Operate operation. Also,
+      // update the number of available threads.
+      std::unique_lock<std::mutex> lock(m_operateQueueMutex);
+      m_threadVector.push_back(
+          std::thread(operateThread, std::ref(*this), m_operateQueue.front()));
+      --m_numberOfAvailableThreads;
+      m_operateQueue.pop();
+      continue;
     }
 
-    //If something happened on the master socket ,
-    //then its an incoming connection
-    if (FD_ISSET(m_sockfd, &readfds))
-    {
-        //accept the communication
-        int new_socket, addrlen{sizeof(m_connectAddress)};
-        if ((new_socket = accept(m_sockfd, (struct sockaddr *)&m_connectAddress,
-                                 (socklen_t *)&addrlen)) < 0)
-        {
-            close(m_sockfd);
-            throw std::system_error{errno, std::system_category()};
-        }
+    // If the Read queue is not empty, there is a Read operation to do.
+    if (!m_readQueue.empty()) {
+      // If the server is serial, then call for the Read operation
+      // in the queue using the main thread.
+      if (m_isSerial == true) {
+        m_readQueue.front().reader.get()->readMessage(
+            *this, m_readQueue.front().parameters);
+        m_readQueue.pop();
+        continue;
+      }
 
-        //add the new communication to the "Wait For Read" Queue
-        ThreadPool::WaitForReadCreator waitFroReadCreator;
-        waitFroReadCreator.addToQueue(*this, {std::time(nullptr), VERSION, new_socket, "FIRST READ", ""});
+      // The server is parallel. Since it's the only class that can create
+      // thread, and according to the wait() function there is a thread
+      // available then create a thread to do the Read operation. Also, update
+      // the number of available threads.
+      std::unique_lock<std::mutex> lock(m_readQueueMutex);
+      m_threadVector.push_back(
+          std::thread(readThread, std::ref(*this), m_readQueue.front()));
+      --m_numberOfAvailableThreads;
+      m_readQueue.pop();
+      continue;
+    }
+  }
+}
+
+void ThreadPool::Queue::wait() {
+  while (true) {
+    // Check if there is an available thread
+    if (m_isSerial == false && m_numberOfAvailableThreads <= 0) {
+      continue;
     }
 
-    {
-        std::unique_lock<std::mutex> lock(m_waitForReadVectorMutex);
-
-        //Check all other waiting reads to see if they have incoming communication
-        for (int i{static_cast<int>(m_waitForReadVector.size() - 1)}; i >= 0; --i)
-        {
-            if (FD_ISSET(m_waitForReadVector[i].sockfd, &readfds))
-            {
-                //add the communication to the "Read" queue
-                m_mapCreator.get()->atReadMap(m_waitForReadVector[i].readType).get()->addToQueue(*this, {m_waitForReadVector[i].operateToCreate, m_waitForReadVector[i].version, m_waitForReadVector[i].sockfd});
-                m_waitForReadVector.erase(m_waitForReadVector.begin() + i);
-            }
-        }
+    if (m_numberOfAvailableThreads >= m_numberOfThreads) {
+      throw std::runtime_error(
+          "number of available operation is bigger then number of threads");
     }
+
+    // there is a Read operation to do.
+    if (!m_readQueue.empty()) {
+      return;
+    }
+
+    // there is an Operate operation to do.
+    if (!m_operateQueue.empty()) {
+      return;
+    }
+
+    // there is a Write operation to do.
+    if (!m_writeQueue.empty()) {
+      return;
+    }
+
+    checkActiveFd();
+
+    if (m_isSerial == true) {
+      joinThreads();
+    }
+  }
 }
 
-void ThreadPool::readThread(Queue &queue, const read read)
-{
-    read.reader.get()->readMessage(queue, read.parameters);
+void ThreadPool::Queue::checkActiveFd() {
+  fd_set readfds;
+  // clear the socket set
+  FD_ZERO(&readfds);
+
+  // add master socket to set
+  FD_SET(m_sockfd, &readfds);
+  int maxSd{m_sockfd};
+
+  // get the current time
+  std::time_t currentTime = std::time(nullptr);
+
+  {
+    std::unique_lock<std::mutex> lock(m_waitForReadVectorMutex);
+    for (int i{static_cast<int>(m_waitForReadVector.size() - 1)}; i >= 0; --i) {
+      // Check if 5 seconds have passed.
+      // If so, close the connection
+      if (m_waitForReadVector[i].lastReadTime + TIME_TO_CLOSE <= currentTime) {
+        m_waitForReadVector.erase(m_waitForReadVector.begin() + i);
+        close(m_waitForReadVector[i].sockfd);
+        continue;
+      }
+
+      // add to read list
+      FD_SET(m_waitForReadVector[i].sockfd, &readfds);
+
+      // highest file descriptor number, need it for the select function
+      if (m_waitForReadVector[i].sockfd > maxSd) {
+        maxSd = m_waitForReadVector[i].sockfd;
+      }
+    }
+  }
+
+  // wait for an activity on one of the sockets , timeout is 0 ,
+  // so select() returns immediately
+  timeval timeout = {0, 0};
+  int activity = select(maxSd + 1, &readfds, NULL, NULL, &timeout);
+
+  if (activity < 0) {
+    closeServer();
+    throw std::system_error{errno, std::system_category()};
+  }
+
+  // If something happened on the master socket ,
+  // then its an incoming connection
+  if (FD_ISSET(m_sockfd, &readfds)) {
+    // accept the communication
+    int new_socket, addrlen{sizeof(m_connectAddress)};
+    if ((new_socket = accept(m_sockfd, (struct sockaddr *)&m_connectAddress,
+                             (socklen_t *)&addrlen)) < 0) {
+      closeServer();
+      throw std::system_error{errno, std::system_category()};
+    }
+
+    // add the new communication to the "Wait For Read" Queue
+    ThreadPool::WaitForReadCreator waitFroReadCreator;
+    waitFroReadCreator.addToQueue(
+        *this, {std::time(nullptr), VERSION, new_socket, "FIRST READ", ""});
+  }
+
+  {
+    std::unique_lock<std::mutex> lock(m_waitForReadVectorMutex);
+
+    // Check all other waiting reads to see if they have incoming communication
+    for (int i{static_cast<int>(m_waitForReadVector.size() - 1)}; i >= 0; --i) {
+      if (FD_ISSET(m_waitForReadVector[i].sockfd, &readfds)) {
+        // add the communication to the "Read" queue
+        m_mapCreator.get()
+            ->atReadMap(m_waitForReadVector[i].readType)
+            .get()
+            ->addToQueue(*this, {m_waitForReadVector[i].operateToCreate,
+                                 m_waitForReadVector[i].version,
+                                 m_waitForReadVector[i].sockfd});
+        m_waitForReadVector.erase(m_waitForReadVector.begin() + i);
+      }
+    }
+  }
 }
 
-void ThreadPool::writeThread(Queue &queue, const write write)
-{
-    write.writer.get()->writeMessage(queue, write.parameters);
+void ThreadPool::readThread(Queue &queue, const read read) {
+  read.reader.get()->readMessage(queue, read.parameters);
 }
 
-void ThreadPool::operateThread(Queue &queue, const operate operate)
-{
-    operate.operation.get()->operate(queue, operate.parameters);
+void ThreadPool::writeThread(Queue &queue, const write write) {
+  write.writer.get()->writeMessage(queue, write.parameters);
 }
 
-void ThreadPool::Queue::joinThreads()
-{
+void ThreadPool::operateThread(Queue &queue, const operate operate) {
+  operate.operation.get()->operate(queue, operate.parameters);
+}
 
-    for_each(m_threadVector.begin(), m_threadVector.end(), [this](std::thread &t1) {
-        if (t1.joinable())
-        {
-            t1.join();
-            ++m_numberOfAvailableThreads;
-        }
-    });
+void ThreadPool::Queue::joinThreads() {
+
+  for_each(m_threadVector.begin(), m_threadVector.end(),
+           [this](std::thread &t1) {
+             if (t1.joinable()) {
+               t1.join();
+               ++m_numberOfAvailableThreads;
+             }
+           });
+}
+
+void ThreadPool::Queue::closeServer() {
+  while (!m_threadVector.empty()) {
+    joinThreads();
+  }
+
+  for (uint i = 0; i < m_readQueue.size(); ++i) {
+    close(m_readQueue.front().parameters.sockfd);
+    m_readQueue.pop();
+  }
+
+  for (uint i = 0; i < m_writeQueue.size(); ++i) {
+    close(m_writeQueue.front().parameters.sockfd);
+    m_writeQueue.pop();
+  }
+
+  for (uint i = 0; i < m_operateQueue.size(); ++i) {
+    close(m_operateQueue.front().parameters.sockfd);
+    m_operateQueue.pop();
+  }
+
+  std::for_each(m_waitForReadVector.begin(), m_waitForReadVector.end(),
+                [this](waitForRead &object) { close(object.sockfd); });
+  m_waitForReadVector.clear();
+
+  close(m_sockfd);
 }
--- src/ThreadPool/ThreadPool.h
+++ src/ThreadPool/ThreadPool.h
@@ -7,100 +7,99 @@
 
 #include "../Factory/MapCreator.h"
 
+#include <atomic>
+#include <mutex>
 #include <queue>
 #include <thread>
 #include <vector>
-#include <atomic>
-#include <mutex>
 
-namespace MapCreator
-{
-    class MapCreator;
+namespace MapCreator {
+class MapCreator;
 }
 
-namespace ThreadPool
-{
+namespace ThreadPool {
 
-    class Queue
-    {
-    public:
-        /**
-     * @brief Construct a new Queue object
-     * 
-     * @param sockfd the socket file descriptor
-     * @param connectAddress the connection address - used when doing accept
-     * @param numberOfThreads number of threads to support the operation.
-     *                        if it equals to 1, then the server is serial.
-     */
-        explicit Queue(const int sockfd, const sockaddr_in connectAddress, const unsigned int numberOfThreads);
-        /**
-         * @brief waits until there is an Read/Operation/Write to do. Once there is,
-         * it does it.
-         * 
-         */
-        void allocate();
-        std::unique_ptr<MapCreator::MapCreator> m_mapCreator;
+class Queue {
+public:
+  /**
+   * @brief Construct a new Queue object
+   *
+   * @param sockfd the socket file descriptor
+   * @param connectAddress the connection address - used when doing accept
+   * @param numberOfThreads number of threads to support the operation.
+   *                        if it equals to 1, then the server is serial.
+   */
+  explicit Queue(const int sockfd, const sockaddr_in connectAddress,
+                 const unsigned int numberOfThreads);
+  /**
+   * @brief waits until there is an Read/Operation/Write to do. Once there is,
+   * it does it.
+   *
+   */
+  void allocate();
+  std::unique_ptr<MapCreator::MapCreator> m_mapCreator;
 
-    private:
-        static constexpr int TIME_TO_CLOSE = 5;
-        static constexpr double VERSION = 1.0;
+private:
+  static constexpr int TIME_TO_CLOSE = 5;
+  static constexpr double VERSION = 1.0;
 
-        /**
-     * @brief only return when there is Read/Operation/Write to do.
-     * If one of the open file descriptors has something to send
-     * then it calls for the Read constructor to add something to the read queue.
-     * If more than 5 seconds have passed, it closes the file descriptors
-     * 
-     */
-        void wait();
+  /**
+   * @brief only return when there is Read/Operation/Write to do.
+   * If one of the open file descriptors has something to send
+   * then it calls for the Read constructor to add something to the read queue.
+   * If more than 5 seconds have passed, it closes the file descriptors
+   *
+   */
+  void wait();
 
-        /**
-         * @brief In this section we check, using the select() function
-         * If there is a read to do in the vector of file descriptors.
-         * If there is, then it calls the Read factory.
-         * 
-         */
-        void checkActiveFd();
+  /**
+   * @brief In this section we check, using the select() function
+   * If there is a read to do in the vector of file descriptors.
+   * If there is, then it calls the Read factory.
+   *
+   */
+  void checkActiveFd();
 
-        void joinThreads();
+  void joinThreads();
+  void closeServer();
 
-        const int m_sockfd;
-        const sockaddr_in m_connectAddress;
-        const unsigned int m_numberOfThreads;
-        const bool m_isSerial;
+  const int m_sockfd;
+  const sockaddr_in m_connectAddress;
+  const unsigned int m_numberOfThreads;
+  const bool m_isSerial;
 
-        std::atomic_bool m_isThereSomethingToRead;
-        std::atomic_uint m_numberOfAvailableThreads;
+  std::atomic_bool m_isThereSomethingToRead;
+  std::atomic_uint m_numberOfAvailableThreads;
 
-        std::queue<read> m_readQueue;
-        std::vector<waitForRead> m_waitForReadVector;
-        std::queue<operate> m_operateQueue;
-        std::queue<write> m_writeQueue;
+  std::queue<read> m_readQueue;
+  std::vector<waitForRead> m_waitForReadVector;
+  std::queue<operate> m_operateQueue;
+  std::queue<write> m_writeQueue;
 
-        std::mutex m_readQueueMutex;
-        std::mutex m_waitForReadVectorMutex;
-        std::mutex m_writeQueueMutex;
-        std::mutex m_operateQueueMutex;
+  std::mutex m_readQueueMutex;
+  std::mutex m_waitForReadVectorMutex;
+  std::mutex m_writeQueueMutex;
+  std::mutex m_operateQueueMutex;
 
-        std::vector<std::thread> m_threadVector;
+  std::vector<std::thread> m_threadVector;
 
-        friend class FirstReadCreator;
-        friend class SecondReadCreator;
+  friend class FirstReadCreator;
+  friend class SecondReadCreator;
 
-        friend class FirstWriteCreator;
-        friend class SecondWriteCreator;
-        friend class ErrorWriteCreator;
+  friend class FirstWriteCreator;
+  friend class SecondWriteCreator;
+  friend class ErrorWriteCreator;
 
-        friend class DFSCreator;
-        friend class BFSCreator;
-        friend class BestFSCreator;
-        friend class AStarCreator;
+  friend class DFSCreator;
+  friend class BFSCreator;
+  friend class BestFSCreator;
+  friend class AStarCreator;
 
-        friend class WaitForReadCreator;
-    };
+  friend class WaitForReadCreator;
+};
 
-    void readThread(Queue &queue, const read read);
-    void writeThread(Queue &queue, const write write);
-    void operateThread(Queue &queue, const operate operate);
+void readThread(Queue &queue, const read read);
+void writeThread(Queue &queue, const write write);
+void operateThread(Queue &queue, const operate operate);
 
 } // namespace ThreadPool
