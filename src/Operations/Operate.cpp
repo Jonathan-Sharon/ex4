c2843d83526a434a2896b89be377ade788daff0a
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-26 07:12:57 +0300

    deleted /** */ type of comment

--- src/Operations/Operate.cpp
+++ src/Operations/Operate.cpp
@@ -8,19 +8,6 @@
 
 #include <iostream>
 
-/**void Operate::DFS::operate(ThreadPool::Queue &queue,
-                           const ThreadPool::operateParameters info) const {
-  Graph::Graph graph{info.readResult};
-  std::vector<std::vector<bool>>(graph.getHeight(),
-                                 std::vector<bool>(graph.getWidth(), false));
-
-  if(graph.)
-  // add the new communication to the "Wait For Read" Queue
-  ThreadPool::SecondWriteCreator secondWriteCreator;
-  secondWriteCreator.addToQueue(queue, {"", info.version, info.sockfd, 0});
-}
-*/
-
 void Operate::BFS::operate(ThreadPool::Queue &queue,
                            const ThreadPool::operateParameters info) const {
   // add the new communication to the "Wait For Read" Queue
