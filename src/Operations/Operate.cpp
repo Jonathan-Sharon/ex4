1f01c257562eacf8ebecc7ee4689019f4c1172d7
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-09-26 07:12:12 +0300

    deleted DFS from the Oeration header

--- src/Operations/Operate.cpp
+++ src/Operations/Operate.cpp
@@ -1,19 +1,31 @@
 #include "Operate.h"
+#include "AStar.h"
+#include "BFS.h"
+#include "BestFS.h"
+#include "DFS.h"
+
+#include "../Operations/Graph/Graph.h"
 
 #include <iostream>
 
-void Operate::DFS::operate(ThreadPool::Queue &queue,
+/**void Operate::DFS::operate(ThreadPool::Queue &queue,
                            const ThreadPool::operateParameters info) const {
+  Graph::Graph graph{info.readResult};
+  std::vector<std::vector<bool>>(graph.getHeight(),
+                                 std::vector<bool>(graph.getWidth(), false));
+
+  if(graph.)
   // add the new communication to the "Wait For Read" Queue
   ThreadPool::SecondWriteCreator secondWriteCreator;
   secondWriteCreator.addToQueue(queue, {"", info.version, info.sockfd, 0});
 }
+*/
 
 void Operate::BFS::operate(ThreadPool::Queue &queue,
                            const ThreadPool::operateParameters info) const {
   // add the new communication to the "Wait For Read" Queue
   ThreadPool::SecondWriteCreator secondWriteCreator;
-  secondWriteCreator.addToQueue(queue, {"yes", info.version, info.sockfd, 0});
+  secondWriteCreator.addToQueue(queue, {"", info.version, info.sockfd, 0});
 }
 
 void Operate::BestFS::operate(ThreadPool::Queue &queue,
--- src/Operations/Operate.h
+++ src/Operations/Operate.h
@@ -3,37 +3,40 @@
 #include "../ThreadPool/StructParameters.h"
 #include "../ThreadPool/ThreadPool.h"
 
-namespace ThreadPool
-{
-    class Queue;
+namespace ThreadPool {
+class Queue;
 }
 
-namespace Operate
+namespace Operate {
+class Operate {
+public:
+  virtual void operate(ThreadPool::Queue &queue,
+                       const ThreadPool::operateParameters info) const = 0;
+  virtual ~Operate() = default;
+};
+
+/**class DFS : public Operate {
+  virtual void operate(ThreadPool::Queue &queue,
+                       const ThreadPool::operateParameters info) const override;
+};
+
+
+class BFS : public Operate
 {
-    class Operate
-    {
-    public:
-        virtual void operate(ThreadPool::Queue &queue, const ThreadPool::operateParameters info) const = 0;
-        virtual ~Operate() = default;
-    };
+    virtual void operate(ThreadPool::Queue &queue, const
+ThreadPool::operateParameters info) const override;
+};
 
-    class DFS : public Operate
-    {
-        virtual void operate(ThreadPool::Queue &queue, const ThreadPool::operateParameters info) const override;
-    };
+class BestFS : public Operate
+{
+    virtual void operate(ThreadPool::Queue &queue, const
+ThreadPool::operateParameters info) const override;
+};
 
-    class BFS : public Operate
-    {
-        virtual void operate(ThreadPool::Queue &queue, const ThreadPool::operateParameters info) const override;
-    };
-
-    class BestFS : public Operate
-    {
-        virtual void operate(ThreadPool::Queue &queue, const ThreadPool::operateParameters info) const override;
-    };
-
-    class AStar : public Operate
-    {
-        virtual void operate(ThreadPool::Queue &queue, const ThreadPool::operateParameters info) const override;
-    };
+class AStar : public Operate
+{
+    virtual void operate(ThreadPool::Queue &queue, const
+ThreadPool::operateParameters info) const override;
+};
+*/
 } // namespace Operate
